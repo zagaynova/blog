d7116f2de5c3802977774094a9e6a6a3c722dae5
zagaynova <masha.zagaynova@gmail.com>
2017-06-08 11:07:31 +0200

    working log in and sign up

--- cookies_en_sessions.js
+++ cookies_en_sessions.js
@@ -3,17 +3,29 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
+var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
 
 console.log(connectionString)
 
-var sequelize = new Sequelize('bulletinboard', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+
+var sequelize = new Sequelize('blog', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
     define: {
         timestamps: false
     }
 });
+
+
+const Cars = sequelize.define('autos', {
+    kleur: Sequelize.STRING,
+    prijs: Sequelize.INTEGER
+});
+
+
+
+
+
 
 var User = sequelize.define('user', {
     name: Sequelize.STRING,
@@ -23,8 +35,11 @@ var User = sequelize.define('user', {
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//inserting middelware session, defined on top
 app.use(session({
-    secret: 'oh wow very secret much security',
+    secret: 'oh wow very secret much security', //session id made secure
     resave: true,
     saveUninitialized: false
 }));
@@ -33,15 +48,25 @@ app.set('views', './src/views');
 app.set('view engine', 'pug');
 
 app.get('/', function (request, response) {
-    console.log(request.query.message);
-    console.log(request.session.user);
-
-    response.render('index', {
-        message: 'ja', //request.query.message,
-        user: request.session.user
-    });
+    response.render('index');
 });
 
+app.get('/signup', function (request, response) {
+    response.render('signup');
+});
+
+app.post('/addnewuser', function(request, response) {
+ //check bestaand adres, maybe.findOne?   
+    User.create({
+        name: request.body.user,
+        email: request.body.email,
+        password: request.body.password
+    });
+    response.redirect('/');
+});
+
+
+//check if user exists
 app.get('/profile', function (request, response) {
     var user = request.session.user;
     if (user === undefined) {
@@ -54,52 +79,67 @@ app.get('/profile', function (request, response) {
 });
 
 app.post('/login', bodyParser.urlencoded({extended: true}), function (request, response) {
-    if(request.body.email.length === 0) {
-        response.redirect('/?message=' + encodeURIComponent("Please fill out your email address."));
-        return;
-    }
-
-    if(request.body.password.length === 0) {
-        response.redirect('/?message=' + encodeURIComponent("Please fill out your password."));
-        return;
-    }
-
     User.findOne({
         where: {
             email: request.body.email
         }
     }).then(function (user) {
         if (user !== null && request.body.password === user.password) {
-            request.session.user = user;
+            request.session.user = user; //session starts when you log in
             response.redirect('/profile');
         } else {
             response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
         }
-    }, function (error) {
+    }, function (error) { //beter to use .catch ipv deze functie
         response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
     });
 });
 
 app.get('/logout', function (request, response) {
-    request.session.destroy(function(error) {
+    request.session.destroy(function(error) { // finishing session
         if(error) {
             throw error;
         }
         response.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
     })
 });
-
-sequelize.sync({force: true}).then(function () {
+//create the new user
+app.post('/users',bodyParser.urlencoded({extended: true}), function (request, response) {
     User.create({
-        name: "stabbins",
-        email: "yes@no",
-        password: "not_password"
-    }).then(function () {
-        var server = app.listen(3000, function () {
-            console.log('Example app listening on port: ' + server.address().port);
+        name: request.body.name,
+        email: request.body.email,
+        password: request.body.password
+    }).then(function (user) {
+        response.redirect('/users/' + user.id);
+    });
+});
+
+sequelize.sync({force: false}).then(function () { 
+    var server = app.listen(3000, function () {
+        console.log('Example app listening on port: ' + server.address().port);
+    });
+    /*
+    Promise.all([
+        Cars.create({
+            kleur: "zwart",
+            prijs: 800
+        }),
+        Cars.create({
+            kleur: "beige",
+            prijs: 120
+        })
+    ]).then(function(result) {
+        Cars.findAll().then(function(cars) {
+            console.log('showing all cars');
+            console.log(cars.length);
+            cars.forEach(function(car) {
+                console.log(car.kleur);
+                console.log(car.prijs);
+            });
         });
     });
-}, function (error) {
+    */
+}, function (error) { // use .catch
     console.log('sync failed: ');
     console.log(error);
 });
