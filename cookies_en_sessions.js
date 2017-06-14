1e3a931ff83c6b43869e9afc56c9ddfe7cbd198e
zagaynova <masha.zagaynova@gmail.com>
2017-06-14 11:11:50 +0200

    working hash and salt

--- cookies_en_sessions.js
+++ cookies_en_sessions.js
@@ -2,10 +2,7 @@ var Sequelize = require('sequelize');
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-
-var connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/blog';
-
-console.log(connectionString)
+var bcrypt = require('bcrypt'); // encrypt
 
 var sequelize = new Sequelize('blog', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
     host: 'localhost',
@@ -13,11 +10,6 @@ var sequelize = new Sequelize('blog', process.env.POSTGRES_USER, process.env.POS
     define: {
         timestamps: false
     }
-});
-
-const Cars = sequelize.define('autos', {
-    kleur: Sequelize.STRING,
-    prijs: Sequelize.INTEGER
 });
 
 var User = sequelize.define('user', {
@@ -46,7 +38,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-//inserting middelware session, defined on top
+// //inserting middelware session, defined on top
 app.use(session({
     secret: 'oh wow very secret much security', //session id made secure
     resave: true,
@@ -55,6 +47,26 @@ app.use(session({
 
 app.set('views', './src/views');
 app.set('view engine', 'pug');
+
+function showBlogPosts(response, user) {
+    var selection = {};
+    if (user) {
+        selection = { userId: user };
+    }
+    Post.findAll({
+        where: selection,
+        include: [
+            User,
+            { model: Comment, include: User } 
+        ],
+        order: [
+            ['id', 'DESC'],
+            [ Comment, 'id', 'ASC']
+        ]
+    }).then(function (selectedposts) {
+        response.render('allposts', {posts: selectedposts});
+    });
+}
 
 app.get('/', function (request, response) {
     response.render('index');
@@ -73,42 +85,11 @@ app.get('/home', function (request, response) {
 });
 
 app.get('/allposts', function (request, response) {
-    Post.findAll({
-        include: [
-            User,
-            { model: Comment, include: User } 
-        ],
-        order: [
-            ['id', 'DESC'],
-            [ Comment, 'id', 'ASC']
-        ]
-    }).then(function (all) {
-        response.render('allposts', {posts: all});
-    });
-
-   // response.render('/allposts', {posts: testposts});
+    showBlogPosts(response);
 });
 
-
 app.get('/myposts', function (request, response) {
-    console.log(request.session.user)
-    Post.findAll({
-        where: {
-            userId: request.session.user.id
-        },
-        include: [
-            User,
-            { model: Comment, include: User } 
-        ],
-        order: [
-            ['id', 'DESC'],
-            [ Comment, 'id', 'ASC']
-        ]
-    }).then(function (myposts) {
-        response.render('allposts', {posts: myposts});
-    });
-
-   // response.render('/allposts', {posts: testposts});
+    showBlogPosts(response, request.session.user.id);
 });
 
 app.post('/postblog', function (request, response) {
@@ -133,24 +114,27 @@ app.post('/addcomment', function (request, response) {
 });
 
 app.post('/addnewuser', function(request, response) {
- //check bestaand adres, maybe.findOne?   
-    //console.log("reached")
-    User.create({
-        name: request.body.user,
-        email: request.body.email,
-        password: request.body.password
-    }).catch(err => {
-        console.log(err)
-    })
+    bcrypt.hash(request.body.password, 7, function(error, hash) {
+        if (error !== undefined) {
+            console.log(error);
+        } else {
+            User.create({
+                name: request.body.name,
+                email: request.body.email,
+                password: hash
+            }).catch(err => {
+                console.log(err)
+            });
+        }
+    });
     response.redirect('/');
 });
-
 
 //check if user exists
 app.get('/profile', function (request, response) {
     var user = request.session.user;
     if (user === undefined) {
-        response.redirect('/?message=' + encodeURIComponent("Please log in to view your profile."));
+        response.render('index', {'message' : "Please log in to view your profile."});
     } else {
         response.render('profile', {
             user: user
@@ -164,14 +148,22 @@ app.post('/login', bodyParser.urlencoded({extended: true}), function (request, r
             email: request.body.email
         }
     }).then(function (user) {
-        if (user !== null && request.body.password === user.password) {
-            request.session.user = user; //session starts when you log in
-            response.redirect('/profile');
-        } else { //! dit bericht wordt niet op het scherm gelogd, niet duidelijk wat de user verkeerd heeft gedaan
-            response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+        if (user !== null) {
+            bcrypt.compare(request.body.password, user.password, function(error, result){
+                if (error !== undefined){
+                    console.log(error);
+                } else {
+                    if (result) {
+                        request.session.user = user; //session starts when you log in
+                        response.redirect('/profile');
+                    } else { //! dit bericht wordt niet op het scherm gelogd, niet duidelijk wat de user verkeerd heeft gedaan
+                        response.render('index', {'message' : "Invalid email or password."});
+                    }
+                }
+            });
         }
     }, function (error) { //beter to use .catch ipv deze functie
-        response.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+        response.render('index', {'message' : "Invalid email or password."});
     });
 });
 
@@ -180,18 +172,8 @@ app.get('/signout', function (request, response) {
         if(error) {
             throw error;
         }
-        response.redirect('/?message=' + encodeURIComponent("Successfully logged out."));
+        response.render('index', {'message' : "Succesfully logged out."});
     })
-});
-//create the new user
-app.post('/users',bodyParser.urlencoded({extended: true}), function (request, response) {
-    User.create({
-        name: request.body.name,
-        email: request.body.email,
-        password: request.body.password
-    }).then(function (user) {
-        response.redirect('/users/' + user.id);
-    });
 });
 
 sequelize.sync({force: false}).then(function () { 
@@ -199,24 +181,4 @@ sequelize.sync({force: false}).then(function () {
         console.log('Example app listening on port: ' + server.address().port);
     });
 })
-    /*
-    Promise.all([
-        Cars.create({
-            kleur: "zwart",
-            prijs: 800
-        }),
-        Cars.create({
-            kleur: "beige",
-            prijs: 120
-        })
-    ]).then(function(result) {
-        Cars.findAll().then(function(cars) {
-            console.log('showing all cars');
-            console.log(cars.length);
-            cars.forEach(function(car) {
-                console.log(car.kleur);
-                console.log(car.prijs);
-            });
-        });
-    });
-    */
+ 
